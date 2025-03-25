b6d5fb1d9dbf3970fb989c8c0e8b7b59d433c562
abomkdad <abomkdad2@gmail.com>
2025-03-25 03:24:20 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+
+const CACHE_NAME = 'order-system-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => {
+                    return cacheName !== CACHE_NAME;
+                }).map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
